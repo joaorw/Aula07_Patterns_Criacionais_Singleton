1d5ec06821b13e87b902b8e26a4274e939b24d7b
JOAOVICTOR <JOAOVICTOR@JOAOVICTOR-PC>
2018-05-01 20:38:27 -0300

    Aula07_Patterns_Criacionais_Singleton

---
+++ Aula7_Patterns_Criacionais_Singleton/src/Exercicio2/SingletonExercise.java
@@ -0,0 +1,12 @@
+package Exercicio2;
+
+//* João Victor Bonfim Rocha 816118224
+
+public class SingletonExercise {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
